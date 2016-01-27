a7ee3a8c11db77e1e0ae40b32a7e5a9fca54f3be
dylanhills <dylanhills333@gmail.com>
2016-01-27 16:59:40 -0700

    pulled in the rest of the files we needed. Also got a basic version working. Next step is to get the sort by height done

--- bin_packing.py
+++ bin_packing.py
@@ -1,4 +1,5 @@
-import driver
+#import driver
+import random
 
 """
 FIND_SOLUTION:
@@ -18,5 +19,50 @@ RETURNS: a list of tuples that designate the top left corner placement,
 """
 
 def find_solution(rectangles):
-    
-    return driver.find_naive_solution(rectangles)  # a working example!
+    lowest_legal_height = []
+    widthGuess = round(guess_good_width(rectangles))
+
+    print("Width Guess: "+str(widthGuess))
+
+    for i in range (0,widthGuess):
+        lowest_legal_height.append(0)
+    placement = []
+    upper_left_x = 0
+    upper_left_y = 0
+
+    for rectangle in rectangles:
+        cur_rect_width = rectangle[0]
+        cur_rect_height = rectangle[1]
+
+        if(cur_rect_width+upper_left_x>widthGuess):
+            upper_left_x = 0
+
+        upper_left_y = lowest_legal_height_in_width_range(upper_left_x,upper_left_x+cur_rect_width,lowest_legal_height)
+
+        coordinate = (upper_left_x, upper_left_y)   # make a tuple
+        placement.insert(0, coordinate)             # insert tuple at front of list
+        
+        for i in range(upper_left_x,upper_left_x+cur_rect_width):
+            lowest_legal_height[i] = upper_left_y+cur_rect_height
+        upper_left_x = upper_left_x + cur_rect_width
+        
+    placement.reverse()                             # original order
+    return placement
+
+def lowest_legal_height_in_width_range(left,right,llh):
+    #print(str(llh[0]))
+    counter = left
+    min = -1
+    while(counter<=right):
+        if min<llh[counter]:
+            min = llh[counter]
+        counter += 1
+    return min
+
+def guess_good_width(rectangles):
+    guess = 0
+    for rectangle in rectangles:
+        guess+= rectangle[0]
+    guess*=10
+    guess/=len(rectangles)
+    return guess
